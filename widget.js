ad3e0c7500ebebfe99d2ac38ab9731209703322f
Ryan Stanley <stanleyrya@gmail.com>
2020-11-28 18:58:10 -0500

    Adding parameter writing capabilites

--- widget.js
+++ widget.js
@@ -1,11 +1,57 @@
 const scripts = [{
+    type: 'raw',
     name: 'interest-map',
     raw: 'https://raw.githubusercontent.com/bring-larry-to-life/scriptable-widget-interest-map/main/widget.js',
-    forceDownload: true
+    forceDownload: true,
+    storedParameters: {"apiKey": "testtest"}
 }]
 
 for (const script of scripts) {
-    await downloadScript(script)
+    if (script.storedParameters) {
+        const writeSuccess = writeStoredParameters(script.name, script.storedParameters)
+        console.log("Attempted to write parameters for script. Success?: " + writeSuccess);
+    }
+    await download(script);
+}
+
+/**
+ * Attempts to write the file ./storage/name.json
+ * Returns false if it cannot be written.
+ */
+function writeStoredParameters(name, params) {
+    let fm = FileManager.local()
+
+    const thisScriptPath = module.filename
+    const storageDir = thisScriptPath.replace(fm.fileName(thisScriptPath, true), '') + "storage"
+    const parameterFile = name + ".json";
+    const parameterPath = storageDir + "/" + parameterFile;
+
+    if (!fm.fileExists(storageDir)) {
+        console.log("Storage folder does not exist! Creating now.");
+        fm.createDirectory(storageDir);
+    } else if (!fm.isDirectory(storageDir)) {
+        console.error("Storage folder exists but is not a directory!");
+        return false;
+    }
+
+    if (fm.fileExists(parameterPath) && fm.isDirectory(parameterPath)) {
+        console.error("Parameter file is a directory, please delete!");
+        return false;
+    }
+
+    fm.writeString(parameterPath, JSON.stringify(params))
+
+    return true;
+}
+
+async function download(script) {
+    switch(script.type) {
+        case 'raw':
+            await downloadScript(script);
+            break;
+        default:
+            console.error("Cant interpret type. Not downloading file.");
+    }
 }
 
 /**
